0627778a30aca4a469faae014976f9168fd25ef0
harsh <>
2022-03-26 02:15:31 +0530

    add modal

--- src/Authentication/AuthModal.js
+++ src/Authentication/AuthModal.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import { AppBar, Button } from "@material-ui/core";
 
-const AuthModal = () => {
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export default function TransitionsModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div>login</div>
-  )
-}
+    <div>
+      <Button
+        variant="contained"
+        style={{
+          width: 85,
+          height: 40,
+          backgroundColor: "#EEBC1D",
+        }}
+        onClick={
+            handleOpen
+        }
+      >
+        Login
+      </Button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+           <AppBar position="static">
 
-export default AuthModal
+           </AppBar>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
