2d7ac4a1d230cc2ccc49e8bba1ced833a4d0f213
harsh <>
2022-04-04 19:29:28 +0530

    added login

--- src/Authentication/Login.js
+++ src/Authentication/Login.js
@@ -1,11 +1,49 @@
 import React from 'react'
+import { useState } from 'react';
+import { Box, TextField, Button } from '@material-ui/core';
 
 const Login = ({handleClose}) => {
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {}
+
   return (
-    <div>
-    Login
-    </div>
-  )
+    <Box
+      p={3}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+      }}
+    >
+        <TextField 
+            variant='outlined'
+            type='email'
+            label=' Enter Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+        />
+        <TextField 
+            variant='outlined'
+            type='password'
+            label=' Enter Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            fullWidth
+        />
+       
+        <Button
+          variant="contained"
+          size="large"
+          style={{backgroundColor: "#EEBC1D"}}
+          onClick={handleSubmit}
+        >
+          Sign Up
+        </Button>
+    </Box>
+  );
 }
 
 export default Login
